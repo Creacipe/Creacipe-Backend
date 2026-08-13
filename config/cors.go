3a8e7f1ba0e932f9533adeee57df46cd0a8aed71
miqdamsyiamn <miqdamsyiamn@gmail.com>
2026-08-13 19:10:59 +0700

    feat: implement CORS middleware and update build configuration to use app binary

--- config/cors.go
+++ config/cors.go
@@ -13,8 +13,8 @@ func CORSMiddleware() gin.HandlerFunc {
 		"http://localhost:5173",
 		"http://127.0.0.1:5500",
 		"http://localhost:5500",
+		"https://creacipe-backend.onrender.com",
 		"https://creacipe.vercel.app",
-		
 	}
 
 	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
